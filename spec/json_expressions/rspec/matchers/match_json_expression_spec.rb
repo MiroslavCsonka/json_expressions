47b9b2e6bccf957b8ad56e85d292237ee0282e82
Miroslav Csonka <miroslav.csonka@gmail.com>
2016-03-10 20:45:28 +0000

    Update rspec and expectation to newest version

--- spec/json_expressions/rspec/matchers/match_json_expression_spec.rb
+++ spec/json_expressions/rspec/matchers/match_json_expression_spec.rb
@@ -4,32 +4,30 @@ require 'json_expressions/rspec'
 module JsonExpressions
   module RSpec
     describe Matchers, "#match_json_expression" do
-      before(:each) do
-        @expression = {
-          l1_string:   'Hello world!',
-          l1_regexp:   /\A0x[0-9a-f]+\z/i,
-          l1_boolean:  false,
-          l1_module:   Numeric,
-          l1_wildcard: wildcard_matcher,
-          l1_array:    ['l1: Hello world',1,true,nil,wildcard_matcher],
-          l1_object:   {
-            l2_string:   'Hi there!',
-            l2_regexp:   /\A[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}\z/i,
-            l2_boolean:  true,
-            l2_module:   Enumerable,
-            l2_wildcard: wildcard_matcher,
-            l2_array:    ['l2: Hello world',2,true,nil,wildcard_matcher],
-            l2_object:   {
-              l3_string:   'Good day...',
-              l3_regexp:   /\A.*\z/,
-              l3_boolean:  false,
-              l3_module:   String,
-              l3_wildcard: wildcard_matcher,
-              l3_array:    ['l3: Hello world',3,true,nil,wildcard_matcher],
-            }
+      let(:expression) { {
+        l1_string: 'Hello world!',
+        l1_regexp: /\A0x[0-9a-f]+\z/i,
+        l1_boolean: false,
+        l1_module: Numeric,
+        l1_wildcard: wildcard_matcher,
+        l1_array: ['l1: Hello world', 1, true, nil, wildcard_matcher],
+        l1_object: {
+          l2_string: 'Hi there!',
+          l2_regexp: /\A[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}\z/i,
+          l2_boolean: true,
+          l2_module: Enumerable,
+          l2_wildcard: wildcard_matcher,
+          l2_array: ['l2: Hello world', 2, true, nil, wildcard_matcher],
+          l2_object: {
+            l3_string: 'Good day...',
+            l3_regexp: /\A.*\z/,
+            l3_boolean: false,
+            l3_module: String,
+            l3_wildcard: wildcard_matcher,
+            l3_array: ['l3: Hello world', 3, true, nil, wildcard_matcher],
           }
         }
-      end
+      } }
 
       it "works with json hashes" do
         positive_json_hash = {
@@ -82,27 +80,27 @@ module JsonExpressions
           }
         }
 
-        positive_json_hash.should match_json_expression(@expression)
-        ->{ negative_json_hash.should match_json_expression(@expression) }.should raise_error(::RSpec::Expectations::ExpectationNotMetError)
-        negative_json_hash.should_not match_json_expression(@expression)
-        ->{ positive_json_hash.should_not match_json_expression(@expression) }.should raise_error(::RSpec::Expectations::ExpectationNotMetError)
+        expect(positive_json_hash).to match_json_expression(expression)
+        expect{ expect(negative_json_hash).to match_json_expression(expression) }.to raise_error(::RSpec::Expectations::ExpectationNotMetError)
+        expect(negative_json_hash).not_to match_json_expression(expression)
+        expect{ expect(positive_json_hash).not_to match_json_expression(expression) }.to raise_error(::RSpec::Expectations::ExpectationNotMetError)
       end
 
       it "works with JSON strings" do
         positive_json_string = '{"l1_string":"Hello world!","l1_regexp":"0xC0FFEE","l1_boolean":false,"l1_module":1.1,"l1_wildcard":true,"l1_array":["l1: Hello world",1,true,null,false],"l1_object":{"l2_string":"Hi there!","l2_regexp":"1234-5678-1234-5678","l2_boolean":true,"l2_module":[1,2,3,4],"l2_wildcard":"Whatever","l2_array":["l2: Hello world",2,true,null,"Whatever"],"l2_object":{"l3_string":"Good day...","l3_regexp":"","l3_boolean":false,"l3_module":"This is like... inception!","l3_wildcard":null,"l3_array":["l3: Hello world",3,true,null,[]]}}}'
         negative_json_string = '{"l1_string":"Hello world!","l1_regexp":"0xC0FFEE","l1_boolean":false,"l1_module":1.1,"l1_wildcard":true,"l1_array":["l1: Hello world",1,true,null,false],"l1_object":{"l2_string":"Hi there!","l2_regexp":"1234-5678-1234-5678","l2_boolean":true,"l2_module":[1,2,3,4],"l2_wildcard":"Whatever","l2_array":["l2: Hello world",2,true,null,"Whatever"],"l2_object":{"l3_string":"Good day...","l3_regexp":"","l3_boolean":false,"l3_module":"This is like... inception!","l3_wildcard":null,"l3_array":["***THIS SHOULD BREAK THINGS***",3,true,null,[]]}}}'
 
-        positive_json_string.should match_json_expression(@expression)
-        ->{ negative_json_string.should match_json_expression(@expression) }.should raise_error(::RSpec::Expectations::ExpectationNotMetError)
-        negative_json_string.should_not match_json_expression(@expression)
-        ->{ positive_json_string.should_not match_json_expression(@expression) }.should raise_error(::RSpec::Expectations::ExpectationNotMetError)
+        expect(positive_json_string).to match_json_expression(expression)
+        expect{ expect(negative_json_string).to match_json_expression(expression) }.to raise_error(::RSpec::Expectations::ExpectationNotMetError)
+        expect(negative_json_string).not_to match_json_expression(expression)
+        expect{ expect(positive_json_string).not_to match_json_expression(expression) }.to raise_error(::RSpec::Expectations::ExpectationNotMetError)
       end
     end
 
     module Matchers
       describe MatchJsonExpression, "#match?" do
-        before(:each) do
-          @matcher = MatchJsonExpression.new({
+        let(:matcher) {
+          MatchJsonExpression.new({
             l1_string:   'Hello world!',
             l1_regexp:   /\A0x[0-9a-f]+\z/i,
             l1_boolean:  false,
@@ -126,7 +124,7 @@ module JsonExpressions
               }
             }
           })
-        end
+        }
 
         it "returns true when passed a matching JSON hash" do
           json = {
@@ -154,7 +152,7 @@ module JsonExpressions
             }
           }
 
-          @matcher.matches?(json).should be_true
+          expect(matcher.matches?(json)).to be true
         end
 
         it "returns false when passed a non-matching JSON hash" do
@@ -183,17 +181,17 @@ module JsonExpressions
             }
           }
 
-          @matcher.matches?(json).should be_false
+          expect(matcher.matches?(json)).to be false
         end
 
         it "returns true when passed a matching JSON string" do
           json_str = '{"l1_string":"Hello world!","l1_regexp":"0xC0FFEE","l1_boolean":false,"l1_module":1.1,"l1_wildcard":true,"l1_array":["l1: Hello world",1,true,null,false],"l1_object":{"l2_string":"Hi there!","l2_regexp":"1234-5678-1234-5678","l2_boolean":true,"l2_module":[1,2,3,4],"l2_wildcard":"Whatever","l2_array":["l2: Hello world",2,true,null,"Whatever"],"l2_object":{"l3_string":"Good day...","l3_regexp":"","l3_boolean":false,"l3_module":"This is like... inception!","l3_wildcard":null,"l3_array":["l3: Hello world",3,true,null,[]]}}}'
-          @matcher.matches?(json_str).should be_true
+          expect(matcher.matches?(json_str)).to be true
         end
 
         it "returns false when passed a non-matching JSON string" do
           json_str = '{"l1_string":"Hello world!","l1_regexp":"0xC0FFEE","l1_boolean":false,"l1_module":1.1,"l1_wildcard":true,"l1_array":["l1: Hello world",1,true,null,false],"l1_object":{"l2_string":"Hi there!","l2_regexp":"1234-5678-1234-5678","l2_boolean":true,"l2_module":[1,2,3,4],"l2_wildcard":"Whatever","l2_array":["l2: Hello world",2,true,null,"Whatever"],"l2_object":{"l3_string":"Good day...","l3_regexp":"","l3_boolean":false,"l3_module":"This is like... inception!","l3_wildcard":null,"l3_array":["***THIS SHOULD BREAK THINGS***",3,true,null,[]]}}}'
-          @matcher.matches?(json_str).should be_false
+          expect(matcher.matches?(json_str)).to be false
         end
       end
     end
--- spec/json_expressions/rspec_spec.rb
+++ spec/json_expressions/rspec_spec.rb
@@ -3,14 +3,7 @@ require 'json_expressions'
 require 'json_expressions/rspec'
 
 describe RSpec do
-  it "includes JsonExpressions::RSpec::Matchers" do
-    modules = ::RSpec.configuration.include_or_extend_modules
-    modules.select! { |(mode,mod,_)| mode == :include }
-    modules.map! { |(mode,mod,_)| mod }
-    modules.should include(::JsonExpressions::RSpec::Matchers)
-  end
-
   it "defines wildcard_matcher" do
-    wildcard_matcher.object_id.should equal ::JsonExpressions::WILDCARD_MATCHER.object_id
+    expect(wildcard_matcher.object_id).to equal ::JsonExpressions::WILDCARD_MATCHER.object_id
   end
 end
